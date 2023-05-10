45f8143b050753e7d92b059b66104d9920456834
Niclas Haderer <niclas.haderer.dev@gmail.com>
2023-05-10 17:23:27 +0200

    feat: added dynamic nextjs build

--- next.config.js
+++ next.config.js
@@ -1,19 +1,26 @@
+const isProduction = process.env.NODE_ENV === "production"
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
+const devOptions = {
   rewrites: async () => [
     {
       source: "/api/:path*",
       destination: "http://127.0.0.1:8080/api/:path*",
     },
   ],
-  output: "standalone",
-  distDir: "dist",
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
+}
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  output: isProduction ? "export" : "standalone",
+  ...(!isProduction && devOptions),
+  distDir: "dist",
 }
 
 module.exports = nextConfig
