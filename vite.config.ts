621952588eba0e6f6ae39d07ee280f7ba57cca8f
Niclas Haderer <niclas.haderer.dev@gmail.com>
2025-01-26 19:44:44 +0100

    Cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "tailwindcss"
 import tsconfigPaths from "vite-tsconfig-paths"
+import { preact } from "@preact/preset-vite"
+
+const shouldUsePreact = !!process.env.USE_PREACT
+const reactPlugin = shouldUsePreact ? preact : react
+console.log("Using preact:", shouldUsePreact)
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths(), reactPlugin()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
